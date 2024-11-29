bd69f43fec92cfc3bd275e89719f8e884a612a3c
John Martinsson <john.martinsson@ri.se>
2024-11-29 11:16:47 +0100

    Adds playable sound icons for different animals.

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-// Add any JavaScript functionality here if needed
+// script.js
+function playSound(id) {
+    var audio = document.getElementById(id);
+    if (audio.paused) {
+        audio.play();
+    } else {
+        audio.pause();
+        audio.currentTime = 0;
+    }
+}
+
+function togglePlayPause(id, element) {
+    var audio = document.getElementById(id);
+    var icon = element.querySelector('.play-pause-icon');
+    if (audio.paused) {
+        audio.play();
+        icon.classList.remove('play');
+        icon.classList.add('pause');
+    } else {
+        audio.pause();
+        audio.currentTime = 0;
+        icon.classList.remove('pause');
+        icon.classList.add('play');
+    }
+}
